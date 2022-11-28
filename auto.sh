8998cceb455e0c4c3f05cee0bb3e75afd80acbd2
itsmeshabeeb <mshabeeb4@gmail.com>
2022-11-28 10:52:39 +0530

    updated

--- auto.sh
+++ auto.sh
@@ -2,8 +2,8 @@
 
 
 python3 manage.py makemigrations
-python3 manage.py migrate 
+python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 #python3 manage.py collectstatic
-sudo systemctl restart apache2
+# sudo systemctl restart apache2
 #sudo systemctl restart gunicorn
